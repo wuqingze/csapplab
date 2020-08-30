d8f93c5052ea23b807bd4bde3a646153935e7740
wuqingze <nongxiaolang@foxmail.com>
2020-08-31 02:05:01 +0800

    超清晰书籍

---
+++ lab2-bomb/mstore.c
@@ -0,0 +1,45 @@
+//int xx(int *);
+//
+//int xx(int *x){
+//    *x += 2;
+//}
+//int test1(int *x){
+//    *x += 1;
+//}
+
+//hello world
+//long mult2(long , long);
+//
+//void multstore(long x, long y, long *desc){
+//    long t = mult2(x, y);
+//    *desc = t;
+//}
+
+//int mult2(int , int);
+//
+//void multstore(int x, int y, int *desc){
+//    int t = mult2(x, y);
+//    *desc = t;
+//}
+//
+
+//char mult2(char , char);
+//
+//void multstore(char x, char y, char *desc){
+//    char t = mult2(x, y);
+//    *desc = t;
+//}
+
+//int mult2(int , int);
+//
+//void multstore(int x, int y, int *desc){
+//    int t = mult2(x, y);
+//    *desc = t;
+//}
+
+float mult2(float , float);
+
+void multstore(float x, float y, float *desc){
+    float t = mult2(x, y);
+    *desc = t;
+}
